bf82498b959ad24140f1cc2f299e2b1fa3ee1005
Pulkit Aggarwal <BashCloud@users.noreply.github.com>
2017-06-04 09:06:37 +0530

    phpDB connection setup

---
+++ verifyData.php
@@ -0,0 +1,30 @@
+<?php
+$dbServerName = "indiastack.database.windows.net";
+$dbUsername = "pulkit";
+$dbPassword = "roorkee@138";
+$dbName = "uid";
+
+// create connection
+$conn = new mysqli($dbServerName, $dbUsername, $dbPassword, $dbName);
+
+// check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+
+
+
+$sql = "SELECT id, name FROM india";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+?> 
